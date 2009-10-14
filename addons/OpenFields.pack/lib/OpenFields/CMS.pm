f2ac0d6c3b10f089af6fecd8d12300ed3c60b00a
Byrne <byrne@majordojo.com>
2009-10-14 01:13:28 -0700

    initial skeleton

--- addons/OpenFields.pack/lib/OpenFields/CMS.pm
+++ addons/OpenFields.pack/lib/OpenFields/CMS.pm
@@ -12,5 +12,56 @@ sub cf_edit {
     return $app->load_tmpl( 'edit.mtml', $param );
 }
 
+sub cf_list {
+    my $app = shift;
+    my ($params) = @_ || {};
+    my $q        = $app->{query};
+    my $blog     = $app->blog;
+
+    $params->{field_saved} = $q->param('field_saved');
+    $params->{screen_id}   = 'list-fields';
+
+    my $code = sub {
+        my ( $obj, $row ) = @_;
+
+        $row->{id}         = $obj->id;
+        $row->{blog_id}    = $obj->blog_id;
+        $row->{label}      = $obj->label;
+
+        my $ts              = $row->{created_on};
+        my $datetime_format = MT::App::CMS::LISTING_DATETIME_FORMAT();
+        my $time_formatted  = format_ts(
+            $datetime_format,
+            $ts,
+            $app->blog || undef,
+            (
+                  $app->user
+                ? $app->user->preferred_language
+                : undef
+            )
+        );
+        $row->{created_on_relative} =
+          relative_date( $ts, time, $app->blog ? $app->blog : undef );
+        $row->{created_on_formatted} = $time_formatted;
+    };
+
+    my $plugin = MT->component('OpenFields');
+
+    $app->listing(
+        {
+            type  => 'field',
+            terms => { blog_id => ( $app->blog ? $app->blog->id : 0 ), },
+            args  => {
+                sort      => 'created_on',
+                direction => 'descend',
+            },
+            listing_screen => 1,
+            code           => $code,
+            template       => $plugin->load_tmpl('list.tmpl'),
+            params         => $params,
+        }
+    );
+}
+
 1;
 __END__
---
+++ addons/OpenFields.pack/lib/OpenFields/FieldTypes.pm
@@ -0,0 +1,16 @@
+package OpenFields::FieldTypes;
+
+use strict;
+
+sub asset {};
+sub cb_boolean {};
+sub cb_multi {};
+sub entry {};
+sub radio {};
+sub select {};
+sub text {};
+sub textarea {};
+sub url {};
+
+1;
+__END__
